d468cb97bf68708fcfc56e5becc43fa0512e9989
Tim Ebbeke <tim.ebbeke@encoway.de>
2021-09-21 16:17:21 +0200

    added two find all tests with const trees

--- tests/find_tests.hpp
+++ tests/find_tests.hpp
@@ -137,3 +137,43 @@ TEST_F(FindTests, CanFindAllElementsBackInStrictlyAscendingOverlappingIntervals)
         ASSERT_NE(tree.find(ival), std::end(tree));
     }
 }
+
+TEST_F(FindTests, CanFindAllOnConstTree)
+{
+    const auto targetInterval = lib_interval_tree::make_safe_interval(16, 21);
+    tree.insert(targetInterval);
+    tree.insert({8, 9});
+    tree.insert({25, 30});
+    std::vector <decltype(tree)::interval_type> intervals;
+    auto findWithConstTree = [&intervals, &targetInterval](auto const& tree)
+    {
+        tree.find_all(targetInterval, [&intervals](auto const& iter) {
+            intervals.emplace_back(*iter);
+            return true;
+        });
+    };
+    findWithConstTree(tree);
+
+    ASSERT_EQ(intervals.size(), 1);
+    EXPECT_EQ(intervals[0], targetInterval);
+}
+
+TEST_F(FindTests, CanOverlapFindAllOnConstTree)
+{
+    const auto targetInterval = lib_interval_tree::make_safe_interval(16, 21);
+    tree.insert(targetInterval);
+    tree.insert({8, 9});
+    tree.insert({25, 30});
+    std::vector <decltype(tree)::interval_type> intervals;
+    auto findWithConstTree = [&intervals, &targetInterval](auto const& tree)
+    {
+        tree.overlap_find_all(targetInterval, [&intervals](auto const& iter) {
+            intervals.emplace_back(*iter);
+            return true;
+        });
+    };
+    findWithConstTree(tree);
+
+    ASSERT_EQ(intervals.size(), 1);
+    EXPECT_EQ(intervals[0], targetInterval);
+}
