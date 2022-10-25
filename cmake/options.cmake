309b9c725191d4bb1d134f28a8a32ad2f68a8ffa
Tim Ebbeke <tim06tr@gmail.com>
2022-10-25 03:24:31 +0200

    Disabled tests by default.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,2 +1,2 @@
 option(INT_TREE_DRAW_EXAMPLES "Draws some examples in a subdirectory. run make_drawable.sh before this" OFF)
-option(INT_TREE_ENABLE_TESTS "Enable tests?" ON)
+option(INT_TREE_ENABLE_TESTS "Enable tests?" OFF)
