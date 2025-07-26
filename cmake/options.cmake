35acb75f24eb2c196aeae4d419cd40f17361fbc9
Tim Ebbeke <Tim06TR@gmail.com>
2025-07-26 15:50:54 +0200

    Added optional type.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,2 +1,4 @@
 option(INT_TREE_DRAW_EXAMPLES "Draws some examples in a subdirectory. run make_drawable.sh before this" OFF)
 option(INT_TREE_ENABLE_TESTS "Enable tests?" OFF)
+# You generally do not want to turn this on, unless you are testing the library.
+option(INT_TREE_USE_OPTIONAL_POLYFILL "Use optional polyfill?" OFF)
